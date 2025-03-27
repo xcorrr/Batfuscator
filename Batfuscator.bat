5e633c6578e03a3585f76098ad0ed1ad69b8147b
xcorr <xcorral.carlos04@gmail.com>
2025-03-27 07:30:57 -0300

    Add files via upload

---
+++ Batfuscator.bat
@@ -0,0 +1,18 @@
+ÿþ
+cls
+@echo off
+if "%~1"=="" exit /b
+if /i not "%~x1"==".bat" if /i not "%~x1"==".cmd" exit /b
+
+where certutil.exe >nul 2>nul || (echo Err: CertUtil not found. & pause & exit /b)
+
+echo //4NCmNscw0K > "temp.~b64"
+
+certutil.exe -f -decode "temp.~b64" "temp_decoded"
+
+del "temp.~b64"
+
+copy /b "temp_decoded" + "%~1" "temp_final"
+move /y "temp_final" "%~1"
+
+del "temp_decoded"
